1a58358feb4f0492839597cc1992b596dbe3b9ee
Chea Socheat <socheat.c@ltlabs.co>
2023-06-07 17:56:11 +0700

    Disable some file

--- src/core/extract.key.js
+++ src/core/extract.key.js
@@ -1,15 +1,15 @@
-const json = {
-    "id": 1,
-    "product": "Admin",
-    "productURL": " https://app-hmg.ltlabs.co/authorized",
-    "betaUrl": null,
-    "productDesc": "Global Admin",
-    "desc2": "",
-    "iconpath": "https://suntex.s3.ap-southeast-1.amazonaws.com/dev/admin/assets/products/GlobalAdminIcon.svg",
-    "product_url": " https://app-hmg.ltlabs.co/authorized?accessToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb21wYW55Q29kZSI6IkhNRyIsImV4cCI6MTY4NjE3NTY4NCwic3ViIjoyMDk2LCJ1c2VySWQiOjIwOTYsInVzZXJuYW1lIjoic3VwZXJhZG1pbiIsImZhY3RvcnlJZCI6NTIwLCJ0eXBlIjoidG9rZW4iLCJpYXQiOjE2ODYxMzI0ODR9.eGhlQK0nzNhMg1st-UtRZt5e2PL1oqBdNkuyIRf8Kf0&refreshToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb21wYW55Q29kZSI6IkhNRyIsInN1YiI6MjA5NiwiZXhwIjoxNjkxMzE2NDg0LCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTY4NjEzMjQ4NH0.FDQlLJSynssYEHV_aPkQpJQ_BoMa52NqYi4zF8qslKg&companyCode=HMG&userId=2096&factoryId=520",
-    "timezone": "GMT+06:00"
-}
+// const json = {
+//     "id": 1,
+//     "product": "Admin",
+//     "productURL": " https://app-hmg.ltlabs.co/authorized",
+//     "betaUrl": null,
+//     "productDesc": "Global Admin",
+//     "desc2": "",
+//     "iconpath": "https://suntex.s3.ap-southeast-1.amazonaws.com/dev/admin/assets/products/GlobalAdminIcon.svg",
+//     "product_url": " https://app-hmg.ltlabs.co/authorized?accessToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb21wYW55Q29kZSI6IkhNRyIsImV4cCI6MTY4NjE3NTY4NCwic3ViIjoyMDk2LCJ1c2VySWQiOjIwOTYsInVzZXJuYW1lIjoic3VwZXJhZG1pbiIsImZhY3RvcnlJZCI6NTIwLCJ0eXBlIjoidG9rZW4iLCJpYXQiOjE2ODYxMzI0ODR9.eGhlQK0nzNhMg1st-UtRZt5e2PL1oqBdNkuyIRf8Kf0&refreshToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb21wYW55Q29kZSI6IkhNRyIsInN1YiI6MjA5NiwiZXhwIjoxNjkxMzE2NDg0LCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTY4NjEzMjQ4NH0.FDQlLJSynssYEHV_aPkQpJQ_BoMa52NqYi4zF8qslKg&companyCode=HMG&userId=2096&factoryId=520",
+//     "timezone": "GMT+06:00"
+// }
 
-Object.entries(json).forEach(([key, value]) => {
-    console.log(`${key}`);
-});
+// Object.entries(json).forEach(([key, value]) => {
+//     console.log(`${key}`);
+// });
--- src/core/sample.test.js
+++ src/core/sample.test.js
@@ -1,34 +1,34 @@
-const axios = require('axios');
-const { expect } = require('chai');
+// const axios = require('axios');
+// const { expect } = require('chai');
 
-describe('CRUD API Testing', () => {
-    let createdResourceId;
+// describe('CRUD API Testing', () => {
+//     let createdResourceId;
 
-    it('should create a new resource', async () => {
-        const payload = { name: 'John Doe', email: 'john@example.com' };
-        const response = await axios.post('https://api.example.com/resources', payload);
-        expect(response.status).to.equal(201);
-        expect(response.data).to.have.property('id');
-        createdResourceId = response.data.id;
-    });
+//     it('should create a new resource', async () => {
+//         const payload = { name: 'John Doe', email: 'john@example.com' };
+//         const response = await axios.post('https://api.example.com/resources', payload);
+//         expect(response.status).to.equal(201);
+//         expect(response.data).to.have.property('id');
+//         createdResourceId = response.data.id;
+//     });
 
-    it('should retrieve the created resource', async () => {
-        const response = await axios.get(`https://api.example.com/resources/${createdResourceId}`);
-        expect(response.status).to.equal(200);
-        expect(response.data).to.have.property('name', 'John Doe');
-        expect(response.data).to.have.property('email', 'john@example.com');
-    });
+//     it('should retrieve the created resource', async () => {
+//         const response = await axios.get(`https://api.example.com/resources/${createdResourceId}`);
+//         expect(response.status).to.equal(200);
+//         expect(response.data).to.have.property('name', 'John Doe');
+//         expect(response.data).to.have.property('email', 'john@example.com');
+//     });
 
-    it('should update the created resource', async () => {
-        const updatedPayload = { name: 'Jane Doe', email: 'jane@example.com' };
-        const response = await axios.put(`https://api.example.com/resources/${createdResourceId}`, updatedPayload);
-        expect(response.status).to.equal(200);
-        expect(response.data).to.have.property('name', 'Jane Doe');
-        expect(response.data).to.have.property('email', 'jane@example.com');
-    });
+//     it('should update the created resource', async () => {
+//         const updatedPayload = { name: 'Jane Doe', email: 'jane@example.com' };
+//         const response = await axios.put(`https://api.example.com/resources/${createdResourceId}`, updatedPayload);
+//         expect(response.status).to.equal(200);
+//         expect(response.data).to.have.property('name', 'Jane Doe');
+//         expect(response.data).to.have.property('email', 'jane@example.com');
+//     });
 
-    it('should delete the created resource', async () => {
-        const response = await axios.delete(`https://api.example.com/resources/${createdResourceId}`);
-        expect(response.status).to.equal(204);
-    });
-});
+//     it('should delete the created resource', async () => {
+//         const response = await axios.delete(`https://api.example.com/resources/${createdResourceId}`);
+//         expect(response.status).to.equal(204);
+//     });
+// });
